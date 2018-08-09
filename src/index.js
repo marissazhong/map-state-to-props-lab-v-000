8e0413067ecacc85d3691ae6b95e525b59aae6de
Marissa Zhong <mwz2101@columbia.edu>
2018-08-09 14:03:40 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -9,10 +9,14 @@ import manageUsers from './reducers/manageUsers'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+const store = createStore(
+  manageUsers, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
-  ,
+  <Provider store={store}>
+    <App store={store}/>
+  </Provider>,
   document.getElementById('root')
 );
